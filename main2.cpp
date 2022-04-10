9ffb27a3c75de44a94d38d8317d063a124e14b8b
Waylon Bader <wbader@hawaii.edu>
2022-04-09 21:28:42 -0400

    Added main2.cpp, just a test bed for the Cat class, not for production

---
+++ main2.cpp
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_animal_farm - EE 205 - Spr 2022
+///
+/// @file main2.cpp
+/// @version 1.0
+///
+/// @author Waylon Bader <wbader@hawaii.edu>
+/// @date   10_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+/// This is just a test bed for the Cat class, not production code
+
+#include "Cat.h"
+
+int main()
+{
+
+
+
+}
