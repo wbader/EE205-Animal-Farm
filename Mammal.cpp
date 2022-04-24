4d650d9fea07098209d0cc404fdedc0b6a84316d
Waylon Bader <wbader@hawaii.edu>
2022-04-24 08:33:06 -0400

    created Mammal.cpp/.h and updated CMakeLists.txt

---
+++ Mammal.cpp
@@ -0,0 +1,11 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_animal_farm - EE 205 - Spr 2022
+///
+/// @file Mammal.cpp
+/// @version 1.0
+///
+/// @author Waylon Bader <wbader@hawaii.edu>
+/// @date   24_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+#include "Mammal.h"
---
+++ Mammal.h
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_animal_farm - EE 205 - Spr 2022
+///
+/// @file Mammal.h
+/// @version 1.0
+///
+/// @author Waylon Bader <wbader@hawaii.edu>
+/// @date   24_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include "Animal.h"
+
+class Mammal : public Animal {
+
+};
