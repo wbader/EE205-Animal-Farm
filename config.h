1767a3d3ab10e8a5e78c50d8f8f763323b4a5751
Waylon Bader <wbader@hawaii.edu>
2022-04-24 08:41:08 -0400

    added enum class color and operator<<

--- config.h
+++ config.h
@@ -82,6 +82,36 @@ inline std::ostream& operator<<( std::ostream& lhs_stream, const Breed& rhs_Bree
 }
 //enum Color  {UNKNOWN_COLOR, BLACK, WHITE, GREY, PINK, RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE};
 
+enum class Color { UNKNOWN_COLOR = 0, CREAM, BLACK, BROWN, CALICO, WHITE) };
+
+inline std::ostream& operator<<( std::ostream& lhs_stream, const Color& rhs_Color ) {
+    switch (rhs_Color) {
+        case Color::UNKNOWN_COLOR:
+            lhs_stream << "Unknown color";
+            break;
+        case Color::CREAM:
+            lhs_stream << "Cream";
+            break;
+        case Color::BLACK:
+            lhs_stream << "Black";
+            break;
+        case Color::BROWN:
+            lhs_stream << "Brown";
+            break;
+        case Color::CALICO:
+            lhs_stream << "Calico";
+            break;
+        case Color::WHITE:
+            lhs_stream << "White";
+            break;
+        default:
+            /// @throw out_of_range If the enum is not mapped to a string.
+            throw std::out_of_range( PROGRAM_NAME ": Color not mapped to a string" );
+    }
+    return lhs_stream;
+}
+
+
 /// Format the heading for dumping members of a class to the console
 ///
 /// Print =====================
